7a6d9289f3bf9869f5d5997fe33db491d9ccb882
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2016-11-09 12:43:02 +0000

    Linted aggressively

--- autoscaling/diff/check_autoscaling.py
+++ autoscaling/diff/check_autoscaling.py
@@ -4,40 +4,57 @@ import boto3
 import pprint
 from fabric.colors import red, yellow, green
 
-VERBOSE=False
+VERBOSE = False
 
-#
+
 def __date2str(dt):
     return(dt.strftime("%Y %m %d %H:%M:%S GMT"))
 
-#
+
 def __check_ag(ag):
     ec2_client = boto3.client('ec2')
     # pp = pprint.PrettyPrinter(indent=4)
     # pp.pprint(ag)
-    print("Checking "+ag['AutoScalingGroupName'])
+    print("Checking " + ag['AutoScalingGroupName'])
     if VERBOSE:
         print("LaunchConfigurationName: " + ag['LaunchConfigurationName'])
     instances = ag['Instances']
     for instance in instances:
         test = ""
-        if instance['LaunchConfigurationName'] != ag['LaunchConfigurationName']:
+        if instance['LaunchConfigurationName'] != ag[
+                'LaunchConfigurationName']:
             an_instance = ec2_client.describe_instances(
                 InstanceIds=[
                     instance['InstanceId'],
                 ],
             )
-            date = __date2str(an_instance['Reservations'][0]['Instances'][0]['LaunchTime'])
+            date = __date2str(an_instance['Reservations'][0][
+                              'Instances'][0]['LaunchTime'])
             test = red(" == DIFFERENT == " + date)
-        if instance['LaunchConfigurationName'] != ag['LaunchConfigurationName']:
-            print(instance['InstanceId'] + ": " + instance['LaunchConfigurationName'] + test)
+        if instance['LaunchConfigurationName'] != ag[
+                'LaunchConfigurationName']:
+            print(
+                instance['InstanceId'] +
+                ": " +
+                instance['LaunchConfigurationName'] +
+                test)
             print("To terminate the instance:")
-            print(yellow("aws autoscaling terminate-instance-in-auto-scaling-group --instance-id " + instance['InstanceId'] + " --no-should-decrement-desired-capacity"))
+            print(
+                yellow(
+                    "aws autoscaling terminate-instance-in-auto-scaling-group --instance-id " +
+                    instance['InstanceId'] +
+                    " --no-should-decrement-desired-capacity"))
             print("# https://docs.aws.amazon.com/cli/latest/reference/autoscaling/terminate-instance-in-auto-scaling-group.html")
         else:
             if VERBOSE:
-                print(instance['InstanceId'] + ": " + instance['LaunchConfigurationName'] + test)
+                print(
+                    instance['InstanceId'] +
+                    ": " +
+                    instance['LaunchConfigurationName'] +
+                    test)
 #
+
+
 def check_single(ag_name):
     client = boto3.client('autoscaling')
     response = client.describe_auto_scaling_groups(
@@ -49,10 +66,14 @@ def check_single(ag_name):
     __check_ag(response['AutoScalingGroups'][0])
 
 #
+
+
 def check_all():
     client = boto3.client('autoscaling')
     response = client.describe_auto_scaling_groups()
-    print(green("Checking " + str(len(response['AutoScalingGroups'])) + " autoscaling roups"))
+    print(green("Checking " +
+                str(len(response['AutoScalingGroups'])) +
+                " autoscaling roups"))
     for ag in response['AutoScalingGroups']:
         __check_ag(ag)
 
--- autoscaling/metrics/check_enabled_metrics_for_ag.py
+++ autoscaling/metrics/check_enabled_metrics_for_ag.py
@@ -6,7 +6,9 @@ from fabric.colors import red, green, yellow, white
 client = boto3.client('autoscaling')
 pp = pprint.PrettyPrinter(indent=4)
 response = client.describe_auto_scaling_groups()
-print(white("Found " + str(len(response['AutoScalingGroups'])) + " autoscaling groups\n"))
+print(white("Found " +
+            str(len(response['AutoScalingGroups'])) +
+            " autoscaling groups\n"))
 
 countDisabled = 0
 countEnabled = 0
@@ -14,21 +16,30 @@ countPartial = 0
 countIgnored = 0
 for anAutoscalingGroup in response['AutoScalingGroups']:
     if len(anAutoscalingGroup['EnabledMetrics']) == 0:
-        if re.search('awseb-e', anAutoscalingGroup['AutoScalingGroupName']) :
-            print(anAutoscalingGroup['AutoScalingGroupName']  + yellow(" BEANSTALK - IGNORING"))
+        if re.search('awseb-e', anAutoscalingGroup['AutoScalingGroupName']):
+            print(
+                anAutoscalingGroup['AutoScalingGroupName'] +
+                yellow(" BEANSTALK - IGNORING"))
             countIgnored = countIgnored + 1
         else:
-            print(anAutoscalingGroup['AutoScalingGroupName']  + red(" DISABLED"))
+            print(
+                anAutoscalingGroup['AutoScalingGroupName'] +
+                red(" DISABLED"))
             countDisabled = countDisabled + 1
     else:
         resultString = len(anAutoscalingGroup['EnabledMetrics'])
         if resultString < 8:
-            resultString = yellow("PARTIALLY DISABLED - ACTIVE: ") + red(str(resultString), bold=False)
+            resultString = yellow(
+                "PARTIALLY DISABLED - ACTIVE: ") + red(str(resultString), bold=False)
             countPartial = countPartial + 1
         else:
             resultString = green(str(resultString))
             countEnabled = countEnabled + 1
-        print(green(anAutoscalingGroup['AutoScalingGroupName']) + " " + resultString)
+        print(
+            green(
+                anAutoscalingGroup['AutoScalingGroupName']) +
+            " " +
+            resultString)
 
 print(white("\nRecap:"))
 print(green("> ENABLED: " + str(countEnabled)))
