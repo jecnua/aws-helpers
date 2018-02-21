97700eb0a4b81260b301958635085de975797f7a
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2018-02-21 16:01:34 +0000

    Refactored the script to filter old servers

--- ec2/old_servers.py
+++ ec2/old_servers.py
@@ -1,42 +1,60 @@
-#
+#!/usr/bin/env python
 
 import boto3
 import pprint
 import datetime
-from operator import itemgetter, attrgetter
-from fabric.colors import red, green, yellow, white
+import argparse
+from operator import itemgetter
 
-PP = pprint.PrettyPrinter(indent=4)
-EC2_CLIENT = boto3.client('ec2')
-RESULT = {}
+pp = pprint.PrettyPrinter(indent=4)
+ec2_client = boto3.client('ec2')
+results = []
+ignored = 0
+total_servers = 0
+
+parser = argparse.ArgumentParser("")
+parser.add_argument('-m', '--max-results', type=int,
+                    default=-1, help='The max number of results')
+parser.add_argument('-o', '--older-than', type=int,
+                    default=0, help='Number of days you want the all_servers to \
+                    be older too')
+args = parser.parse_args()
 
 
-def _search_old_nodes(age_in_days=30, region='us-east-1'):
-    print('Age in days: ' + str(age_in_days))
-    print('Region: ' + region)
-    all_servers = EC2_CLIENT.describe_instances()
-    # PP.pprint(all_servers['Reservations'])
+def _search_old_nodes():
+    global ignored
+    global total_servers
+    all_servers = ec2_client.describe_instances()
     for reservation in all_servers['Reservations']:
         for instance in reservation['Instances']:
+            total_servers += 1
             state = instance['State']['Name']
-            # PP.pprint(instance['State']['Name'])
             lunch_time = instance['LaunchTime']
-            # print(type(lunch_time))
             now = datetime.datetime.now(datetime.timezone.utc)
             delta = now - lunch_time
-            name = 'NO NAME'
-            if 'Tags' in instance:
-                for tag in instance['Tags']:
-                    if tag['Key'] == 'Name':
-                        name = tag['Value']
-            RESULT.update(
-                {instance['InstanceId'] + ' ' + name + ' ' + state : delta.days}
-            )
-            # diff_in_time = datetime.datetime.now() - lunch_time
-            # PP.pprint(instance['InstanceType'] + ' ' + str(instance['LaunchTime']))
-    # PP.pprint(sorted(RESULT, key=itemgetter(2), reverse=True))
-    PP.pprint(sorted(RESULT.items(), key=itemgetter(1), reverse=True))
-    # PP.pprint(sorted(RESULT.values()))
+            if int(delta.days) > args.older_than:
+                name = 'NO NAME'
+                if 'Tags' in instance:
+                    for tag in instance['Tags']:
+                        if tag['Key'] == 'Name':
+                            name = tag['Value']
+                results.append((instance['InstanceId'], name,
+                                delta.days, state))
+            else:
+                ignored += 1
 
 
-_search_old_nodes(age_in_days=30, region='us-east-1')
+print("Gathering the data... This can take a while (AWS is slow)")
+print("FYI: Filters won't speed it up :D")
+_search_old_nodes()
+results.sort(key=itemgetter(2), reverse=True)
+pp.pprint(results[:args.max_results:])
+showing = 0
+if args.max_results == -1:
+    showing = 'all'
+else:
+    showing = args.max_results
+print(f"Found {len(results)} servers that respect the search criteria \
+(showing {showing} results)")
+print(f"Ignoring {ignored} servers because they are younger than give age")
+print(f"Found {total_servers} servers in total")
